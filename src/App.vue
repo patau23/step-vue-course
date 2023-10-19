ba4c7144b4f3a694f6c4438e88e83f46fb5ff0bd
patau23 <akejan_2302@mail.ru>
2023-10-19 16:00:42 +0600

    first commit

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+  data: () => {
+    return {};
+  },
+  computed: {},
+  watch: {},
+  methods: {},
+};
+</script>
+
+<template>
+  <div class="app">App</div>
+</template>
+
+<style scoped></style>
